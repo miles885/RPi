85c47f239883a181b2174d5df4869489c1b17f16
Miles Gapcynski <miles885@gmail.com>
2019-11-05 22:26:08 -0500

    Initial commit of project 8.

---
+++ Project8/gps_reader.py
@@ -0,0 +1,80 @@
+# Python Modules
+import json
+import threading
+
+# 3rd Party Modules
+from gps import *
+
+# Project Modules
+from message_handler import MessageType
+
+class GPSReader(threading.Thread):
+    """
+    Class used for reading GPS data from a sensor
+    """
+
+    def __init__(self, msgQueue, readPeriod=0.5):
+        """
+        Constructor
+
+        @param msgQueue   The queue to place GPS messages on
+        @param readPeriod The time between GPS reads (seconds)
+
+        @return None
+        """
+
+        threading.Thread.__init__(self)
+
+        self.shutdownEvent = threading.Event()
+
+        self._msgQueue = msgQueue
+        self._readPeriod = readPeriod
+
+        # Initialize GPS
+        self._gpsd = gps(mode=WATCH_ENABLE | WATCH_NEWSTYLE)
+    
+    def run(self):
+        """
+        Overriden method called when the thread is started
+
+        @param None
+
+        @return None
+        """
+        
+        gpsData = self.__getGPSData()
+
+        # Broadcast GPS data
+        if gpsData:
+            msgData = json.dumps(gpsData)
+
+            self._msqQueue.puts((msgData, MessageType.GPS_MESSAGE))
+
+        time.sleep(self._readPeriod)
+
+    def __getGPSData(self):
+        """
+        Retrieves GPS data from a USB sensor
+
+        @param None
+
+        @return GPS data (dictionary)
+        """
+
+        gpsData = {}
+
+        data = self._gpsd.next()
+
+        # Filter on the Time Position Velocity class
+        if data['class'] == 'TPV':
+            gpsData['time'] = data.get('time')
+            gpsData['lat'] = data.get('lat')
+            gpsData['lon'] = data.get('lon')
+            gpsData['alt'] = data.get('alt')
+            gpsData['speed'] = data.get('speed')
+            gpsData['climb'] = data.get('climb')
+            gpsData['epy'] = data.get('epy')
+            gpsData['epx'] = data.get('epx')
+            gpsData['epv'] = data.get('epv')
+
+        return gpsData
---
+++ Project8/tcp_sender.py
@@ -0,0 +1,69 @@
+# Python Modules
+import threading
+import time
+
+class TCPSender(threading.Thread):
+    """
+    Periodically sends messages to connected clients
+    """
+
+    def __init__(self, msgQueue, socketList, sockerListMutex, sendPeriod=0.1)
+        """
+        Constructor
+
+        @param msqQueue        The queue to read messages from
+        @param socketList      The socker list used for sending messages
+        @param socketListMutex The mutex used to ensure the socket list is correct
+        @param sendPeriod      The time between checking the queue for messages to send
+
+        @return None
+        """
+
+        threading.Thread.__init__(self)
+
+        self.shutdownEvent = threading.Event()
+
+        self._msqQueue = msgQueue
+        self._socketList = socketList
+        self._socketListMutex = socketListMutex
+        self._sendPeriod = sendPeriod
+    
+    def run(self):
+        """
+        Overriden method called when the thread is started
+
+        @param None
+
+        @return None
+        """
+
+        while not self.shutdownEvent.is_set():
+            while not self._msqQueue.empty():
+                msg = self._msqQueue.get()
+
+                msgData = msg[0]
+                msgType = msg[1]
+
+                self._socketListMutex.acquire()
+
+                try:
+                    for sock in self._socketList:
+                        MessageHandler.sendMsg(sock, msgData, msgType)
+                finally:
+                    self._socketListMutex.release()
+
+            time.sleep(self._sendPeriod)
+        
+        # Cleanup
+        self.__shutdown()
+
+    def __shutdown(self):
+        """
+        Performs shutdown procedures for the thread
+
+        @param None
+
+        @return None
+        """
+
+        pass
