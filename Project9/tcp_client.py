c991f54302d76da7643017c3f1eb5b856b5ea17f
Miles Gapcynski <miles885@gmail.com>
2019-11-21 22:15:56 -0500

    Initial commit of project 9

---
+++ Project9/tcp_client.py
@@ -0,0 +1,177 @@
+# Python Modules
+import json
+import os
+import pprint
+import select
+import signal
+import socket
+import threading
+import time
+
+# Project Modules
+from message_handler import MessageType, MessageHandler
+
+# Globals
+keepRunning = True
+
+class TCPClient(threading.Thread):
+    """
+    Client that establishes socket connections with a server
+    """
+
+    def __init__(self, selectTimeout=3, socketTimeout=5):
+        """
+        Constructor
+
+        @param selectTimeout: The select timeout when checking the socket list
+        @param socketTimeout: The socket timeout
+
+        @return None
+        """
+
+        threading.Thread.__init__(self)
+
+        self.shutdownEvent = threading.Event()
+
+        self._selectTimeout = selectTimeout
+
+        self._clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._clientSocket.settimeout(socketTimeout)
+
+        self._clientSocket.connect(('192.168.1.67', 9000))  # RPi IP on home network
+        #self._clientSocket.connect(('192.168.4.1', 9000))  # RPi wireless access point
+
+    def run(self):
+        """
+        Overriden method called when the thread is started
+
+        @param None
+
+        @return None
+        """
+
+        inputSocketList = []
+        inputSocketList.append(self._clientSocket)
+
+        while not self.shutdownEvent.is_set():
+            readyToRead, readyToWrite, inputError = select.select(inputSocketList, [], [], self._selectTimeout)
+
+            for sock in readyToRead:
+                # Read a message off of the socket
+                msgData = MessageHandler.recvMsg(sock)
+
+                # Process the message
+                if msgData is not None:
+                    self.__processMsg(msgData)
+                else:
+                    break
+
+        # Cleanup
+        self.__shutdown()
+
+    def __processMsg(self, msgData):
+        """
+        Processes a message received from the server
+
+        @param msgData: The message data
+
+        @return None
+        """
+
+        msgType = msgData[0]
+        msg = json.loads(msgData[1])
+
+        if msgType == MessageType.GPS_MESSAGE:
+            time = ''
+            lon = 'NaN'
+            lat = 'NaN'
+            alt = 'NaN'
+            speed = 'NaN'
+            climb = 'NaN'
+            epx = 'NaN'
+            epy = 'NaN'
+            epv = 'NaN'
+
+            if 'time' in msg and msg['time'] is not None:
+                time = msg['time']
+            
+            if 'lon' in msg and msg['lon'] is not None:
+                lon = '%4.6f %s (deg)' % (msg['lon'], 'E' if msg['lon'] > 0 else 'W')
+            
+            if 'lat' in msg and msg['lat'] is not None:
+                lat = '%4.6f %s (deg)' % (msg['lat'], 'N' if msg['lat'] > 0 else 'S')
+            
+            if 'alt' in msg and msg['alt'] is not None:
+                alt = '%4.6f (m)' % msg['alt']
+            
+            if 'speed' in msg and msg['speed'] is not None:
+                speed = '%4.6f (MPH)' % msg['speed']
+            
+            if 'climb' in msg and msg['climb'] is not None:
+                climb = '%4.6f (ft/min)' % msg['climb']
+            
+            # Estimated Longitude Error
+            if 'epx' in msg and msg['epx'] is not None:
+                epx = '+/- %4.6f (m)' % msg['epx']
+            
+            # Estimated Latitude Error
+            if 'epy' in msg and msg['epy'] is not None:
+                epy = '+/- %4.6f (m)' % msg['epy']
+            
+            # Estimated Vertical Error
+            if 'epv' in msg and msg['epv'] is not None:
+                epv = '+/- %4.6f (m)' % msg['epv']
+
+            if os.name == 'nt':
+                os.system('cls')
+            else:
+                os.system('clear')
+
+            print('           Time:  %s' % time)
+            print('      Longitude:  %s' % lon)
+            print('       Latitude:  %s' % lat)
+            print('       Altitude:  %s' % alt)
+            print('          Speed:  %s' % speed)
+            print('          Climb:  %s' % climb)
+            print('Longitude Error:  %s' % epx)
+            print(' Latitude Error:  %s' % epy)
+            print(' Altitude Error:  %s' % epv)
+
+    def __shutdown(self):
+        """
+        Performs shutdown procedures for the thread
+
+        @param None
+
+        @return None
+        """
+
+        self._clientSocket.close()
+
+def service_shutdown(signum, fname):
+    """
+    Handles signals (interrupts)
+
+    @param signum: The signal to be handled
+    @param fname:  The callback function name
+
+    @return None
+    """
+
+    global keepRunning
+
+    keepRunning = False
+
+if __name__ == "__main__":
+    # Register a signal handler
+    signal.signal(signal.SIGINT, service_shutdown)
+
+    # Start the TCP client
+    tcpClient = TCPClient()
+    tcpClient.start()
+
+    # Keep alive
+    while keepRunning:
+        time.sleep(1)
+
+    tcpClient.shutdownEvent.set()
---
+++ Project9/tcp_server.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+
+# Python Modules
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+
+import select
+import signal
+import socket
+import time
+import threading
+
+# Project Modules
+from gps_reader import GPSReader
+from message_handler import MessageHandler
+from rpy_reader import RPYReader
+from tcp_sender import TCPSender
+
+# Globals
+keepRunning = True
+
+class TCPServer(threading.Thread):
+    """
+    Server that establishes socket connections between the server and clients
+    """
+
+    def __init__(self, serverAddress='0.0.0.0', serverPort=9000, backLog=1, selectTimeout=5):
+        """
+        Constructor
+
+        @param serverAddress: The server address
+        @param serverPort:    The server port
+        @param backLog:       Number of unaccepted connections allowed 
+                              before refusing new connections
+        @param selectTimeout: The select timeout when checking the socket list (seconds)
+
+        @return None
+        """
+
+        threading.Thread.__init__(self)
+
+        self.shutdownEvent = threading.Event()
+
+        self._selectTimeout = selectTimeout
+
+        # Create a server socket to listen for connections
+        self._serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        self._serverSocket.bind((serverAddress, serverPort))
+        self._serverSocket.listen(backLog)
+
+        # Add the server socket to the socket list
+        self._socketList = []
+        self._socketList.append(self._serverSocket)
+
+        self._socketListMutex = threading.Lock()
+        self._msqQueue = queue.Queue()
+
+        # Create TCP sender
+        self._tcpSender = TCPSender(self._msqQueue, self._serverSocket, self._socketList, self._socketListMutex)
+        self._tcpSender.start()
+
+        # Create GPS reader
+        self._gpsReader = GPSReader(self._msqQueue)
+        self._gpsReader.start()
+
+        # Create RPY reader
+        self._rpyReader = RPYReader(self._msqQueue)
+        self._rpyReader.start()
+
+    def run(self):
+        """
+        Overriden method called when the thread is started
+
+        @param None
+
+        @return None
+        """
+
+        print('Listening for client connections...')
+
+        while not self.shutdownEvent.is_set():
+            readyToRead, readyToWrite, inputError = select.select(self._socketList, [], [], self._selectTimeout)
+
+            # Iterate over input sockets
+            for sock in readyToRead:
+                # Received new connection request
+                if sock is self._serverSocket:
+                    print('Received connection request. Establishing connection with client.')
+
+                    # Accept the connection and append it to the socket list
+                    clientSocket, address = self._serverSocket.accept()
+
+                    #TODO: Add this if there's a timeout blocking issue, or make the sockets non-blocking
+                    #clientSocket.settimeout(0.5)
+
+                    self._socketListMutex.acquire()
+
+                    try:
+                        self._socketList.append(clientSocket)
+                    finally:
+                        self._socketListMutex.release()
+                # Received message from client
+                else:
+                    # Read a message off of the socket
+                    msgData = MessageHandler.recvMsg(sock)
+
+                    # Process the message
+                    if msgData is not None:
+                        self.__processMsg(sock, msgData)
+                    # The client disconnected
+                    else:
+                        print('Client disconnected')
+
+                        self._socketListMutex.acquire()
+
+                        try:
+                            self._socketList.remove(sock)
+                        finally:
+                            self._socketListMutex.release()
+
+                        sock.close()
+
+        # Cleanup
+        self.__shutdown()
+
+    def __processMsg(self, sock, msgData):
+        """
+        Processes a message received from a client
+
+        @param sock:    The client socket
+        @param msgData: The message data
+
+        @return None
+        """
+
+        pass
+
+    def __shutdown(self):
+        """
+        Performs shutdown procedures for the thread
+
+        @param None
+
+        @return None
+        """
+
+        self._rpyReader.shutdownEvent.set()
+        self._gpsReader.shutdownEvent.set()
+        self._tcpSender.shutdownEvent.set()
+
+        self._rpyReader.join()
+        self._gpsReader.join()
+        self._tcpSender.join()
+
+        self._serverSocket.close()
+
+def service_shutdown(signum, fname):
+    """
+    Handles signals (interrupts)
+
+    @param signum: The signal to be handled
+    @param fname:  The callback function name
+
+    @return None
+    """
+
+    global keepRunning
+
+    keepRunning = False
+
+if __name__ == "__main__":
+    # Register a signal handler
+    signal.signal(signal.SIGINT, service_shutdown)
+
+    # Start the TCP server
+    tcpServer = TCPServer()
+    tcpServer.start()
+
+    # Keep alive
+    while keepRunning:
+        time.sleep(1)
+
+    tcpServer.shutdownEvent.set()
